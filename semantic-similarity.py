f99c31fe77a401ed790057cf99366e9735e9ab87
Michael Chary <mac389@gmail.com>
2013-12-05 19:12:34 -0500

    simulation with all 1000 positive controls

--- semantic-similarity.py
+++ semantic-similarity.py
@@ -1,40 +1,48 @@
-import numpy as np
+import itertools
 
 from nltk.corpus import wordnet
+from nltk import word_tokenize, pos_tag
 from pprint import pprint
 from time import time
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 fname = "positive-control_.txt"
 with open(fname) as f:
 	corpus = [string.strip() 
 			for string in f.readlines()[0].split(',')]
 
-corpus = corpus[:40]
+#corpus = corpus[:100]
 similarity = np.zeros((len(corpus),len(corpus)))
-#Can make this much more efficient
 #TODO filter out stop words
 #TODO filter out words with low tf-idf
-#TODO use optional POS argument
 
 start = time()
+morphy_tag = {'NN':wordnet.NOUN,'JJ':wordnet.ADJ,'VB':wordnet.VERB,'RB':wordnet.ADV}
+listify = lambda item: item if type(item) is type([]) else list(item)
 for i in xrange(len(corpus)):
+ 	print i/float(len(corpus))
+ 	if i/float(len(corpus))%0.1 == 0:
+ 		print chr(27) + "[2J"
 	for j in xrange(i):
-	 	one = corpus[i].split()
-	 	two = corpus[j].split()
 	 	s = 0
-	 	for first_word in one:
-	 		for second_word in two:
-	 			if first_word == second_word:
+	 	for one in pos_tag(word_tokenize(corpus[i])):
+	 		for two in pos_tag(word_tokenize(corpus[j])):
+	 			#First item of tuple is word, second is part of speech
+	 			if one[0] == two[0]:
 	 				sim = 1
 	 			else:
-		 			#hack for now
-		 			if len(wordnet.synsets(first_word)) > 0 and len(wordnet.synsets(second_word)) > 0 :
-			 			first_synset = wordnet.synsets(first_word)[0]
-		 				second_synset = wordnet.synsets(second_word)[0]
-
-		 				sim = first_synset.path_similarity(second_synset)
+	 				sim = 0
+		 			synsets = {}
+		 			synsets['one'] = listify(wordnet.synsets(one[0],pos=morphy_tag[one[1]] if one[1] in morphy_tag else wordnet.NOUN))
+		 			synsets['two'] = listify(wordnet.synsets(two[0],pos=morphy_tag[two[1]] if two[1] in morphy_tag else wordnet.NOUN))
+		 			if len(synsets['one'])>0 and len(synsets['two'])>0:
+		 				tmp = 0
+			 			for a,b in itertools.product(synsets['one'],synsets['two']):
+			 				tmp += a.path_similarity(b) if a.path_similarity(b) else 0
+		 				tmp /= float(sum(map(len,synsets.items())))
+		 				sim += tmp
 	 				else:
 	 					sim = 0
 			s += sim if sim else 0
