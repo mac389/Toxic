a864e0d8d63df33c4ba7381af28b7c593b359e27
Michael Chary <mac389@gmail.com>
2013-12-13 16:39:28 -0500

    Moved test file to main folder, renamed to test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+import json
+from time import time
+
+filename = 'semantic-distance-database.json'
+READ = 'rb'
+WRITE = 'wb'
+db = json.load(open(filename,READ))
+
+from SemanticString import SemanticString
+from nltk.corpus import wordnet
+
+test_string = "Colorless green ideas sleep furiously."
+other_string = "I like drug stores."
+start = time()
+
+d1 = SemanticString(test_string,db)
+d2 = SemanticString(other_string,db)
+
+print d1-d2
+print d1
+print d2
+json.dump(db,open(filename,WRITE))	
+print time()-start
